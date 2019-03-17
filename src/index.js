cc21cdbc96cd5132c7156c880e3384936156f406
Danil Lukonin <OldBlackTree@gmail.com>
2019-03-17 11:41:33 +0300

    style: formating

--- src/index.js
+++ src/index.js
@@ -13,17 +13,19 @@ module.exports = function makeExchange(currency) {
     if (currency <= 0) {
         return {};
     }
+    
     if (currency > 10000) {
-        return {error: "You are rich, my friend! We don't have so much coins for exchange"}
+        return {error: `You are rich, my friend! We don't have so much coins for exchange`}
     }
 
     for (let key in existingMoney) {
-
         let floor = Math.floor(currency / existingMoney[key]);
         currency = currency - floor * existingMoney[key];
+       
         if (floor) {
             exchange[key] = floor;
         }
     }
+    
     return exchange;
 }
